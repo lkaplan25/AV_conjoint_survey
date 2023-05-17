09ea48f5838b07eff18e7ccec60a1d6f812a80be
emse-p4a-gwu <john.helveston@gmail.com>
2023-05-16 21:00:35 -0400

    update WTP figs

--- code/4analysis.R
+++ code/4analysis.R
@@ -1,13 +1,87 @@
+# Visualize results of estimated mixed logit WTP space model
+
 # Load libraries and settings
 source(here::here('code', '0setup.R'))
 
 # Load models
-# load(here::here("models", "mxl.RData"))
-load(here::here("models", "mxl_travelTime_by_mode.RData"))
-load(here::here("models", "mxl_gender.RData"))
-load(here::here("models", "mxl_income.RData"))
+mxl_wtp <- readRDS(here::here("models", "mxl_wtp.Rds"))
+mxl_wtp_income_low <- readRDS(here::here("models", "mxl_wtp_income_low.Rds"))
+mxl_wtp_income_high <- readRDS(here::here("models", "mxl_wtp_income_high.Rds"))
+mxl_wtp_gender_male <- readRDS(here::here("models", "mxl_wtp_gender_male.Rds"))
+mxl_wtp_gender_female <- readRDS(here::here("models", "mxl_wtp_gender_female.Rds"))
+                                                                                          
+# Function to compute mode variables from draws
 
-# Visualize results of estimated mixed logit WTP space model
+compute_wtp_vars <- function(draws) {
+  draws <- draws %>% 
+    mutate(
+      wtp_bus_autoYes = mode_bus + bus_automated_yes,
+      wtp_bus_autoYes_attendantYes = mode_bus + bus_automated_yes + bus_attendant_yes,
+      wtp_RH_autoYes = mode_RH + RH_automated_yes,
+      wtp_RH_autoYes_attendantYes = mode_RH + RH_automated_yes + RH_attendant_yes,
+      wtp_sharedRH_autoYes = mode_sharedRH + sharedRH_automated_yes,
+      wtp_sharedRH_autoYes_attendantYes = mode_sharedRH + sharedRH_automated_yes + sharedRH_attendant_yes
+  )
+  return(draws)
+}
+
+# Function to obtain a formatted data frame of mode coefficients for plotting
+
+get_df_mode <- function(wtp_ci) {
+  
+  wtp_mode <- wtp_ci %>% 
+    filter(par %in% c(
+      'mode_bus', 'wtp_bus_autoYes', 'wtp_bus_autoYes_attendantYes',
+      'mode_RH', 'wtp_RH_autoYes', 'wtp_RH_autoYes_attendantYes',
+      'mode_sharedRH','wtp_sharedRH_autoYes', 'wtp_sharedRH_autoYes_attendantYes'
+    ))
+  
+  # Create data frames for plotting each attribute:
+  #   level   = The attribute level (x-axis)
+  #   utility = The utility associated with each level (y-axis)
+  
+  df_mode <- wtp_mode %>% 
+    mutate(
+      mode = case_when(
+        par %in% c('mode_bus', 'mode_RH', 'mode_sharedRH') ~ "Not\nAutomated",
+        par %in% c(
+          'wtp_bus_autoYes', 'wtp_RH_autoYes','wtp_sharedRH_autoYes'
+        ) ~ "Automated,\nNo Attendant\nPresent",
+        par %in% c(
+          'wtp_bus_autoYes_attendantYes', 'wtp_RH_autoYes_attendantYes',
+          'wtp_sharedRH_autoYes_attendantYes'
+        ) ~ "Automated,\nAttendant\nPresent"
+      ),
+      par = recode_factor(
+        wtp_mode$par, 
+        "mode_bus" = "Bus", 
+        "mode_RH" = "Ride-hailing", 
+        "mode_sharedRH" = "Shared Ride-hailing",
+        'wtp_bus_autoYes' = "Bus", 
+        'wtp_RH_autoYes' = "Ride-hailing",
+        'wtp_sharedRH_autoYes' = "Shared Ride-hailing",
+        'wtp_bus_autoYes_attendantYes' = "Bus", 
+        'wtp_RH_autoYes_attendantYes' = "Ride-hailing",
+        'wtp_sharedRH_autoYes_attendantYes' = "Shared Ride-hailing"
+      )
+    )
+  return(df_mode)
+}
+
+# Plot theme function 
+
+plot_theme <- function() {
+  return(
+    theme_bw() + 
+    theme(
+      text = element_text(size=14, family = "Roboto Condensed", face = "bold"),
+      strip.text.y = element_text(angle = 0),
+      axis.title.y = element_blank(),
+      axis.title.x = element_text(size = 12),
+      plot.subtitle = element_text(face = "plain", size = 12)
+    )
+  )
+}
 
 # -----------------------------------------------------------------------------
 # Get WTP estimates with 95% CI
@@ -18,48 +92,16 @@ covariance <- vcov(mxl_wtp)
 wtp_draws <- as.data.frame(MASS::mvrnorm(10^4, coefs, covariance))
 
 # Computing the combinations of WTP draws
-wtp_draws <- wtp_draws %>% 
-  mutate(
-    wtp_bus_autoYes = mode_bus + bus_automated_yes,
-    wtp_bus_autoYes_attendantYes = mode_bus + bus_automated_yes + bus_attendant_yes,
-    wtp_RH_autoYes = mode_RH + RH_automated_yes,
-    wtp_RH_autoYes_attendantYes = mode_RH + RH_automated_yes + RH_attendant_yes,
-    wtp_sharedRH_autoYes = mode_sharedRH + sharedRH_automated_yes,
-    wtp_sharedRH_autoYes_attendantYes = mode_sharedRH + sharedRH_automated_yes + sharedRH_attendant_yes
-  )
+wtp_draws <- compute_wtp_vars(wtp_draws)
+wtp_ci <- ci(wtp_draws)
+wtp_ci <- wtp_ci[-1,] # Drop lambda (we won't plot this)
+wtp_ci
 
-wtp_ci2 <- ci(wtp_draws)
-wtp_ci2 <- wtp_ci2[-1,] # Drop lambda (we won't plot this)
-wtp_ci2
-
-# -----------------------------------------------------------------------------
 # Plot results
-
-wtp_ci <- wtp_ci2 
 
 # Separate coefficient CIs by attribute 
 wtp_ci$par <- row.names(wtp_ci)
-wtp_travelTime <- wtp_ci %>% filter(par == 'travelTime')
-wtp_mode <- wtp_ci %>% filter(par %in% c('mode_bus', 'wtp_bus_autoYes', 'wtp_bus_autoYes_attendantYes',
-                                         'mode_RH', 'wtp_RH_autoYes', 'wtp_RH_autoYes_attendantYes',
-                                         'mode_sharedRH','wtp_sharedRH_autoYes', 'wtp_sharedRH_autoYes_attendantYes'))
-
-# Create data frames for plotting each attribute:
-#   level   = The attribute level (x-axis)
-#   utility = The utility associated with each level (y-axis)
-
-df_mode <- wtp_mode %>% 
-  mutate(mode = case_when(
-    par %in% c('mode_bus', 'mode_RH', 'mode_sharedRH') ~ "Not\nAutomated",
-    par %in% c('wtp_bus_autoYes', 'wtp_RH_autoYes','wtp_sharedRH_autoYes') ~ "Automated,\nNo Attendant\nPresent",
-    par %in% c('wtp_bus_autoYes_attendantYes', 'wtp_RH_autoYes_attendantYes','wtp_sharedRH_autoYes_attendantYes') ~ "Automated,\nAttendant\nPresent"
-  ),
-  par = recode_factor(wtp_mode$par, "mode_bus" = "Bus", "mode_RH" = "Ride-hailing", "mode_sharedRH" = "Shared Ride-hailing",
-                      'wtp_bus_autoYes' = "Bus", 'wtp_RH_autoYes' = "Ride-hailing",'wtp_sharedRH_autoYes' = "Shared Ride-hailing",
-                      'wtp_bus_autoYes_attendantYes' = "Bus", 'wtp_RH_autoYes_attendantYes' = "Ride-hailing",'wtp_sharedRH_autoYes_attendantYes' = "Shared Ride-hailing")
-  )
-
-
+df_mode <- get_df_mode(wtp_ci)
 
 # Get upper and lower bounds (plots should have the same x-axis)
 xmin <- floor(min(df_mode$lower))
@@ -73,32 +115,30 @@ plot_mode_automated_attendant_All <- df_mode %>%
   geom_errorbar(width = 0.3, color = 'navyblue') +
   facet_grid(mode~., scales = "free_y", space = "free") +
   scale_x_continuous(limits = c(xmin, xmax)) +
-  labs(y = NULL, x = 'Willingness to Pay ($1) relative to rail',
-       title = "AV preferences shift with addition of an attendant",
-       subtitle = "Automation alone does not drastically alter mode preferences") +
-  theme_bw() +
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  theme(
-    text = element_text(size=14, family = "Fira Sans", face = "bold"),
-    strip.text.y = element_text(angle = 0),
-    axis.title.y = element_blank(),
-    axis.title.x = element_text(size = 12),
-    plot.subtitle = element_text(face = "plain", size = 12)
+  labs(
+    y = NULL, 
+    x = 'Willingness to Pay ($1) relative to rail',
+    title = "AV preferences shift with addition of an attendant",
+    subtitle = "Automation alone does not drastically alter mode preferences"
   ) +
-  scale_x_continuous(limits = c(xmin, xmax)) 
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  plot_theme()
 
 plot_mode_automated_attendant_All
 
-
-# ggsave(
-#   filename = here('figs', 'wtp_mode.png'), 
-#   plot = plot_mode_automated_attendant_All, 
-#   width = 7, height = 2.75
-# )
+ggsave(
+  filename = here('figs', 'wtp_mode.png'),
+  plot = plot_mode_automated_attendant_All,
+  width = 7, height = 4
+)
 
 
 ## Subgroup analysis-------------
-### Gender/Income---------
+
+### Gender---------
+
+mxl_wtp_A <- mxl_wtp_gender_male
+mxl_wtp_B <- mxl_wtp_gender_female
 
 # Get WTP estimates with 95% CI
 
@@ -106,119 +146,142 @@ plot_mode_automated_attendant_All
 coefs_A <- coef(mxl_wtp_A)
 covariance_A <- vcov(mxl_wtp_A)
 coef_draws_A <- as.data.frame(MASS::mvrnorm(10^4, coefs_A, covariance_A))
-
 coefs_B <- coef(mxl_wtp_B)
 covariance_B <- vcov(mxl_wtp_B)
 coef_draws_B <- as.data.frame(MASS::mvrnorm(10^4, coefs_B, covariance_B))
 
 # Computing the combinations of WTP draws
-coef_draws_A <- coef_draws_A %>% 
-  mutate(
-    wtp_bus_autoYes = mode_bus + bus_automated_yes,
-    wtp_bus_autoYes_attendantYes = mode_bus + bus_automated_yes + bus_attendant_yes,
-    wtp_RH_autoYes = mode_RH + RH_automated_yes,
-    wtp_RH_autoYes_attendantYes = mode_RH + RH_automated_yes + RH_attendant_yes,
-    wtp_sharedRH_autoYes = mode_sharedRH + sharedRH_automated_yes,
-    wtp_sharedRH_autoYes_attendantYes = mode_sharedRH + sharedRH_automated_yes + sharedRH_attendant_yes
-  )
-
-wtp_ci_A <- ci(coef_draws_A, ci = 0.95)
-wtp_ci_A <- wtp_ci_A[-1,] %>% # Drop lambda (we won't plot this)
-  mutate(
-    #group = "Male"
-    group = "Mid/High Income"
-  )
+coef_draws_A <- compute_wtp_vars(coef_draws_A)
+wtp_ci_A <- ci(coef_draws_A)
+wtp_ci_A <- wtp_ci_A[-1,] 
+wtp_ci_A$group <- "Male"
 wtp_ci_A$par <- row.names(wtp_ci_A)
 
-coef_draws_B <- coef_draws_B%>% 
-  mutate(
-    wtp_bus_autoYes = mode_bus + bus_automated_yes,
-    wtp_bus_autoYes_attendantYes = mode_bus + bus_automated_yes + bus_attendant_yes,
-    wtp_RH_autoYes = mode_RH + RH_automated_yes,
-    wtp_RH_autoYes_attendantYes = mode_RH + RH_automated_yes + RH_attendant_yes,
-    wtp_sharedRH_autoYes = mode_sharedRH + sharedRH_automated_yes,
-    wtp_sharedRH_autoYes_attendantYes = mode_sharedRH + sharedRH_automated_yes + sharedRH_attendant_yes
-  )
-
+coef_draws_B <- compute_wtp_vars(coef_draws_B)
 wtp_ci_B <- ci(coef_draws_B)
-wtp_ci_B <- wtp_ci_B[-1,] %>% # Drop lambda (we won't plot this)
-  mutate(
-    #group = "Female",
-    group = "Low Income"
-  )
+wtp_ci_B <- wtp_ci_B[-1,] 
+wtp_ci_B$group <- "Female"
 wtp_ci_B$par <- row.names(wtp_ci_B)
-
 
 # Rejoin data frames
 
-wtp_groups  <-  wtp_ci_A %>% 
-  full_join(wtp_ci_B, by = c('mean', 'lower', 'upper', 'group', 'par'))
-
+wtp_groups  <-  rbind(wtp_ci_A, wtp_ci_B) 
 
 # Plot results
 
-# Separate coefficient CIs by attribute 
-wtp_mode <- wtp_groups %>% filter(par %in% c('mode_bus', 'wtp_bus_autoYes', 'wtp_bus_autoYes_attendantYes', 
-                                             'mode_RH', 'wtp_RH_autoYes', 'wtp_RH_autoYes_attendantYes', 
-                                             'mode_sharedRH','wtp_sharedRH_autoYes', 'wtp_sharedRH_autoYes_attendantYes'))
-
-# Create data frames for plotting each attribute:
-#   level   = The attribute level (x-axis)
-#   utility = The utility associated with each level (y-axis)
-
-df_mode <- wtp_mode %>% 
-  mutate(mode = case_when(
-    par %in% c('mode_bus', 'mode_RH', 'mode_sharedRH') ~ "Not\nAutomated",
-    par %in% c('wtp_bus_autoYes', 'wtp_RH_autoYes','wtp_sharedRH_autoYes') ~ "Automated,\nNo Attendant\nPresent",
-    par %in% c('wtp_bus_autoYes_attendantYes', 'wtp_RH_autoYes_attendantYes','wtp_sharedRH_autoYes_attendantYes') ~ "Automated,\nAttendant\nPresent"
-  ),
-  par = recode_factor(wtp_mode$par, "mode_bus" = "Bus", "mode_RH" = "Ride-hailing", "mode_sharedRH" = "Shared Ride-hailing",
-                      'wtp_bus_autoYes' = "Bus", 'wtp_RH_autoYes' = "Ride-hailing",'wtp_sharedRH_autoYes' = "Shared Ride-hailing",
-                      'wtp_bus_autoYes_attendantYes' = "Bus", 'wtp_RH_autoYes_attendantYes' = "Ride-hailing",'wtp_sharedRH_autoYes_attendantYes' = "Shared Ride-hailing")
-  )
-
-
+wtp_mode <- get_df_mode(wtp_groups)
 
 # Get upper and lower bounds (plots should have the same x-axis)
-xmin <- floor(min(df_mode$lower))
-xmax <- ceiling(max(df_mode$upper))
+xmin <- floor(min(wtp_mode$lower))
+xmax <- ceiling(max(wtp_mode$upper))
 
 # Plot the WTP for each mode *with 95% CI*
 
 plotColors = c("#F16814", "#3690BF")
 
-plot_mode_automated_attendant <- df_mode %>% 
-  ggplot(aes(x = par, y = mean, ymin = lower, ymax = upper, group = group, color = group)) +
-  geom_point(size = 1.5, position=position_dodge(width = .5)) +
-  geom_errorbar(width = 0.5, position=position_dodge(width = .5)) +
-  coord_flip() +
-  facet_grid(mode~., scales = "free_y", space = "free") +
-  labs(x = NULL, y = 'Willingness to Pay ($1) relative to rail'
-       #subtitle = "Men willing to pay more than women for automation + attendant"
-       ) +
-  scale_y_continuous(limits = c(xmin, xmax)) +
-  theme_bw() +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  theme(
-    text = element_text(size=13, family = "Fira Sans", face = "bold"),
-    strip.text.y = element_text(angle = 0),
-    axis.title.y = element_blank(),
-    axis.title.x = element_text(size = 12),
-    legend.title=element_blank(),
-    #legend.position="none"
+plot_mode_automated_attendant_gender <- wtp_mode %>% 
+  ggplot(
+    aes(
+      y = par, x = mean, xmin = lower, xmax = upper,  
+      group = group, color = group
+    )
   ) +
-  scale_color_manual(
-    values = plotColors) 
-
-plot_mode_automated_attendant
+  geom_point(size = 1.5, position = position_dodge(width = .5)) +
+  geom_errorbar(width = 0.5, position = position_dodge(width = .5)) +
+  facet_grid(mode~., scales = "free_x", space = "free") +
+  scale_x_continuous(limits = c(xmin, xmax)) +
+  scale_color_manual(values = plotColors) +
+  labs(
+    y = NULL, 
+    x = 'Willingness to Pay ($1) relative to rail'
+    #subtitle = "Men willing to pay more than women for automation + attendant"
+  ) +
+    geom_vline(xintercept = 0, linetype = "dashed") +
+  plot_theme() 
+  
+plot_mode_automated_attendant_gender
 
 # Save plot 
 
-# ggsave(
-#   filename = here::here('figs', 'mxl_wtp_income.png'),
-#   plot = plot_mode_automated_attendant,
-#   width = 7, height = 2.75
-# )
+ggsave(
+  filename = here::here('figs', 'mxl_wtp_gender.png'),
+  plot = plot_mode_automated_attendant_gender,
+  width = 7, height = 3.75
+)
+
+### Income---------
+
+mxl_wtp_A <- mxl_wtp_income_high
+mxl_wtp_B <- mxl_wtp_income_low
+
+# Get WTP estimates with 95% CI
+
+# Take 10,000 draws of the coefficients of each model
+coefs_A <- coef(mxl_wtp_A)
+covariance_A <- vcov(mxl_wtp_A)
+coef_draws_A <- as.data.frame(MASS::mvrnorm(10^4, coefs_A, covariance_A))
+coefs_B <- coef(mxl_wtp_B)
+covariance_B <- vcov(mxl_wtp_B)
+coef_draws_B <- as.data.frame(MASS::mvrnorm(10^4, coefs_B, covariance_B))
+
+# Computing the combinations of WTP draws
+coef_draws_A <- compute_wtp_vars(coef_draws_A)
+wtp_ci_A <- ci(coef_draws_A)
+wtp_ci_A <- wtp_ci_A[-1,] 
+wtp_ci_A$group <- "Mid/High Income"
+wtp_ci_A$par <- row.names(wtp_ci_A)
+
+coef_draws_B <- compute_wtp_vars(coef_draws_B)
+wtp_ci_B <- ci(coef_draws_B)
+wtp_ci_B <- wtp_ci_B[-1,] 
+wtp_ci_B$group <- "Low Income"
+wtp_ci_B$par <- row.names(wtp_ci_B)
+
+# Rejoin data frames
+
+wtp_groups <- rbind(wtp_ci_A, wtp_ci_B) 
+
+# Plot results
+
+wtp_mode <- get_df_mode(wtp_groups)
+
+# Get upper and lower bounds (plots should have the same x-axis)
+xmin <- floor(min(wtp_mode$lower))
+xmax <- ceiling(max(wtp_mode$upper))
+
+# Plot the WTP for each mode *with 95% CI*
+
+plotColors = c("#F16814", "#3690BF")
+
+plot_mode_automated_attendant_income <- wtp_mode %>% 
+  ggplot(
+    aes(
+      y = par, x = mean, xmin = lower, xmax = upper,  
+      group = group, color = group
+    )
+  ) +
+  geom_point(size = 1.5, position = position_dodge(width = .5)) +
+  geom_errorbar(width = 0.5, position = position_dodge(width = .5)) +
+  facet_grid(mode~., scales = "free_x", space = "free") +
+  scale_x_continuous(limits = c(xmin, xmax)) +
+  scale_color_manual(values = plotColors) +
+  labs(
+    y = NULL, 
+    x = 'Willingness to Pay ($1) relative to rail'
+  ) +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  plot_theme() 
+
+plot_mode_automated_attendant_income
+
+# Save plot 
+
+ggsave(
+  filename = here::here('figs', 'mxl_wtp_income.png'),
+  plot = plot_mode_automated_attendant_income,
+  width = 7, height = 3.75
+)
+
 
 
 
